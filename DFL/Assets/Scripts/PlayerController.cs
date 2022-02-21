ea238a47446ad0e9f25b00309ae0a21b3d7d4231
DHS5 <tomndha@gmail.com>
2022-02-21 22:52:52 +0100

    Player Controller upgrade commit
    - Upgraded player controller
    --> move on the sides
    --> accelerate
    --> decelerate

--- DFL/Assets/Scripts/PlayerController.cs
+++ DFL/Assets/Scripts/PlayerController.cs
@@ -9,12 +9,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Tooltip("Body of the player")]
-    [SerializeField]
-    private GameObject playerBody;
+    [SerializeField]  private GameObject playerBody;
 
     [Tooltip("Camera attached to the player")]
-    [SerializeField]
-    private GameObject playerCamera;
+    [SerializeField] private GameObject playerCamera;
 
     [Tooltip("Rigidbody of the player")]
     private Rigidbody playerRigidbody;
@@ -22,28 +20,71 @@ public class PlayerController : MonoBehaviour
     [Tooltip("Velocity of the player")]
     private Vector3 velocity;
 
-    [Tooltip("Horizontal speed of the player")]
-    private float speed;
+    [Tooltip("Forward speed of the player")]
+    [SerializeField] private float speed = 10f;
+
+    [Tooltip("Side speed multiplier of the player")]
+    [SerializeField] private float sideSpeedM = 5f;
+    private float SideSpeed
+    {
+        get { return Input.GetAxis("Horizontal") * sideSpeedM; }
+        set { sideSpeedM = value; }
+    }
+
+    [Tooltip("Side speed multiplier of the player")]
+    [SerializeField] private float accelerationM = 5f;
+    private float Acceleration
+    {
+        get
+        {
+            float acc = Input.GetAxis("Vertical");
+            if (acc <= 0) return acc * accelerationM;
+            else
+            {
+                if (canAccelerate) { Invoke(nameof(CantAccelerate), 2f); return acc * accelerationM; }
+                else return 0;
+            }
+        }
+        set { accelerationM = value; }
+    }
+
+    [Tooltip("Is the player able to accelerate")]
+    private bool canAccelerate = true;
+
+    /// <summary>
+    /// Disable the player's acceleration during 10 seconds
+    /// </summary>
+    private void CantAccelerate()
+    {
+        canAccelerate = false;
+        Invoke(nameof(CanAccelerate), 10f);
+    }
+    /// <summary>
+    /// Enable the player's acceleration
+    /// </summary>
+    private void CanAccelerate()
+    {
+        canAccelerate = true;
+    }
 
     /// <summary>
     /// Called on the player's creation
     /// </summary>
     void Start()
     {
-        // Get the rigidbody component of the player
-        playerRigidbody = GetComponent<Rigidbody>();
 
-        speed = 1f;
+        // Sets the player parameter
+        
     }
 
     private void Update()
     {
-        velocity = Vector3.forward * speed * Time.deltaTime;
+        velocity = Vector3.forward * (speed + Acceleration) * Time.deltaTime + Vector3.right * SideSpeed * Time.deltaTime;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        playerRigidbody.AddForce(velocity, ForceMode.Force);
+        transform.Translate(velocity);
     }
 }
